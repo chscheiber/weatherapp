c7d105a28c5812936ca9d2760b4135348b3c4738
Christoph Scheiber <chrisscheiber@gmx.at>
2020-11-24 21:02:53 +0100

    Init

---
+++ app/src/main/java/com/example/bsaweatherapp/GetDrawableResource.java
@@ -0,0 +1,25 @@
+package com.example.bsaweatherapp;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetDrawableResource {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static int get(WeatherData data, String name) {
+        Map<String, Integer> icons = new HashMap<>();
+        icons.put("01n", R.drawable._01n);
+        icons.put("02n", R.drawable._02n);
+        icons.put("03n", R.drawable._03n);
+        //TODO: ICONS downloaden und einfügen https://openweathermap.org/weather-conditions
+        int id = R.drawable._01n;
+        try {
+            id = icons.get(data.getIcon());
+        }
+        catch (NullPointerException e){
+            Log.e(LOG_TAG, "Error", e);
+        }
+        return id;
+    }
+}
---
+++ app/src/main/java/com/example/bsaweatherapp/MainActivity.java
@@ -0,0 +1,202 @@
+package com.example.bsaweatherapp;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String RESULT_STRING = "result";
+    private Random mRandom = new Random();
+    private ListAdapter mAdapter;
+    private List<WeatherData> weatherData = new LinkedList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.rv_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ListAdapter(weatherData);
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.setOnClickListener(new ListAdapter.ListItemClickListener() {
+            @Override
+            public void onListItemClick(WeatherData item) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DetailActivity.VALUE_KEY, item);
+                startActivity(intent);
+            }
+        });
+
+        Button generateButton = findViewById(R.id.btn_generate);
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadWebResult();
+//                List<String> data = generateContent();
+//                mAdapter.swapData(data);
+            }
+        });
+
+        loadWebResult();
+    }
+
+    private void loadWebResult() {
+        try {
+            URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?q=Wien&units=metric&appid=231b88bacecc85e7d47891a53667356f");
+            new LoadWebContentTask().execute(url);
+        } catch (MalformedURLException e) {
+//            outputTextView.setText(R.string.url_error);
+            Log.e(LOG_TAG, "URL Error", e);
+        }
+    }
+
+    private String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setConnectTimeout(5000);
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput)
+                return scanner.next();
+            else
+                return null;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    private class LoadWebContentTask extends AsyncTask<URL, Void, List<WeatherData>> {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        protected List<WeatherData> doInBackground(URL... urls) {
+            URL url = urls[0];
+            String resultString = "";
+            try {
+                resultString = getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                Log.e(LOG_TAG,  "IO Error", e);
+            }
+            if (resultString != null && resultString.length() != 0) {
+                try {
+                    List<WeatherData> weatherDataAll = new LinkedList<>();
+                    JSONObject jsonRoot = new JSONObject(resultString);
+                    JSONArray items = jsonRoot.getJSONArray("list");
+                    for (int i = 0; i < items.length(); i++) {
+                        // TODO: Surround everything with try catch
+                        JSONObject item = items.getJSONObject(i);
+                        int dt = item.getInt("dt");
+                        double temp = item.getJSONObject("main").getDouble("temp");
+                        String icon = item.getJSONArray("weather").getJSONObject(0).getString("icon");
+                        String condition = item.getJSONArray("weather").getJSONObject(0).getString("main");
+                        String pressure = item.getJSONObject("main").getString("pressure") + "%";
+                        String humidity = item.getJSONObject("main").getString("humidity") + "%";
+                        String cloudCover = item.getJSONObject("clouds").getString("all") + "%";
+                        String windSpeed = item.getJSONObject("wind").getString("speed") + "kph";
+                        String windDirection = item.getJSONObject("wind").getString("deg") + "°";
+
+                        String rain = "No rain";
+                        String snow = "No snow";
+                        try {
+                            rain = item.getJSONObject("rain").getString("rain.3h") + "mm";
+                            snow = item.getJSONObject("snow").getString("snow.3h") + "mm";
+                        }
+                        catch (JSONException ex) {
+                            Log.e(LOG_TAG, "JSON Error", ex);
+                        }
+
+                        WeatherData weatherData = new WeatherData(dt, icon, temp, condition,
+                                pressure, humidity, cloudCover, windSpeed, windDirection, rain, snow);
+                        weatherDataAll.add(weatherData);
+                    }
+                    return weatherDataAll;
+                } catch (JSONException ex) {
+                    Log.e(LOG_TAG, "JSON Error", ex);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<WeatherData> result) {
+            super.onPostExecute(result);
+            if (result != null && result.size() > 0) {
+                weatherData.clear();
+                weatherData.addAll(result);
+                mAdapter.swapData(result);
+            }
+            else {
+                Log.e(LOG_TAG,  "Result empty");
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<String> generateContent() {
+        List<String> data = new LinkedList<>();
+        for (int i = 0; i < 200; i++)
+            data.add(String.valueOf(mRandom.nextInt(10000)));
+        return data;
+    }
+}
